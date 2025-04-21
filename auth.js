49b1a678e2ac73eee55d36427dc88aed0e759fee
ViktorieKob <138300887+ViktorieKob@users.noreply.github.com>
2025-04-21 20:55:57 +0200

    Add files via upload

---
+++ auth.js
@@ -0,0 +1,21 @@
+// auth.js
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
+import { auth } from "./firebase.js";
+
+window.login = function () {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const errorP = document.getElementById("error");
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then(() => {
+      if (email.toLowerCase() === "vedouci@test.cz") {
+        window.location.href = "zadosti.html";
+      } else {
+        window.location.href = "dashboard.html";
+      }
+    })
+    .catch((error) => {
+      errorP.textContent = "Chyba přihlášení: " + error.message;
+    });
+};
